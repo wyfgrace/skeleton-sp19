f059b1f18783b6cd1337bc1e13222e3ad9297a2c
wyfgrace <wyfgrace2108@gmail.com>
2019-08-19 22:59:53 -0700

    add "final" before variables

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -2,56 +2,56 @@ import java.util.function.DoubleBinaryOperator;
 
 public class NBody {
     public static double readRadius(String file_name) {
-        In in = new In (file_name);
+        final In in = new In(file_name);
         in.readInt();
         return in.readDouble();
     }
 
-
     public static Body[] readBodies(String file_name) {
-        In in = new In (file_name);
-        int num = in.readInt();
-        double radius = in.readDouble();
-        Body[] result = new Body[num];
+        final In in = new In(file_name);
+        final int num = in.readInt();
+        final double radius = in.readDouble();
+        final Body[] result = new Body[num];
         int n = 0;
-        while (! in.isEmpty() && n<num){
-            double xxpoint = in.readDouble();
-            double yypoint = in.readDouble();
-            double xxvel = in.readDouble();
-            double yyvel = in.readDouble();
-            double planet_mass = in.readDouble();
-            String planet_name = in.readString();
-            result[n] = new Body(xxpoint, yypoint, xxvel, yyvel,planet_mass,planet_name);
-            n = n+1;
+        while (!in.isEmpty() && n < num) {
+            final double xxpoint = in.readDouble();
+            final double yypoint = in.readDouble();
+            final double xxvel = in.readDouble();
+            final double yyvel = in.readDouble();
+            final double planet_mass = in.readDouble();
+            final String planet_name = in.readString();
+            result[n] = new Body(xxpoint, yypoint, xxvel, yyvel, planet_mass, planet_name);
+            n = n + 1;
         }
         return result;
     }
+
     public static void main(String args[]) {
-        double T = Double.parseDouble(args[0]);
-        double dt = Double.parseDouble(args[1]);
-        String filename = args[2];
-        Body[] bodies = readBodies(filename);
-        double universeRadius = readRadius(filename);
+        final double T = Double.parseDouble(args[0]);
+        final double dt = Double.parseDouble(args[1]);
+        final String filename = args[2];
+        final Body[] bodies = readBodies(filename);
+        final double universeRadius = readRadius(filename);
 
         StdDraw.enableDoubleBuffering();
         double time = 0;
         while (time < T) {
-            double[] xForce = new double[bodies.length];
-            double[] yForce = new double[bodies.length];
-            for(int i = 0; i<bodies.length; i ++) {
+            final double[] xForce = new double[bodies.length];
+            final double[] yForce = new double[bodies.length];
+            for (int i = 0; i < bodies.length; i++) {
                 xForce[i] = bodies[i].calcNetForceExertedByX(bodies);
                 yForce[i] = bodies[i].calcNetForceExertedByY(bodies);
             }
-            for (int i = 0; i< bodies.length; i ++) {
+            for (int i = 0; i < bodies.length; i++) {
                 bodies[i].update(dt, bodies[i].calcNetForceExertedByX(bodies), bodies[i].calcNetForceExertedByY(bodies));
             }
             time = time + dt;
 
             StdDraw.clear();
-            StdDraw.setScale(- universeRadius, universeRadius);
+            StdDraw.setScale(-universeRadius, universeRadius);
             StdDraw.picture(0, 0, "images/starfield.jpg");
 
-            for (Body body: bodies) {
+            for (Body body : bodies) {
                 body.draw();
             }
             StdDraw.show();
@@ -65,5 +65,4 @@ public class NBody {
                     bodies[i].yyVel, bodies[i].mass, bodies[i].imgFileName);
         }
     }
-
 }
